a441d1d73f4d9d4bb40232e345e39d3b2c55a9e2
mkarmark <mikarmar@microsoft.com>
2023-12-05 22:46:32 +0000

    try now

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -1,28 +1,30 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
-namespace StaticWebAppsEndToEndTesting.GetMessage
+namespace Company.Function
 {
-    public static class GetMessage
+    public class GetMessage
     {
-        [FunctionName("GetMessage")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ExecutionContext context,
-            ILogger log)
+        private readonly ILogger _logger;
+
+        public GetMessage(ILoggerFactory loggerFactory)
         {
-            log.LogInformation("C# HTTP trigger function processed a request! :) ");
-            string message = File.ReadAllText(context.FunctionAppDirectory + "/content.txt");
-            return new OkObjectResult(message);
+            _logger = loggerFactory.CreateLogger<GetMessage>();
+        }
+
+        [Function("GetMessage")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
         }
     }
 }
